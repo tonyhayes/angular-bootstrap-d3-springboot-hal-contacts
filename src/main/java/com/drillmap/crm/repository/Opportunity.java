908ce1c23fb7d9c67bfa2d3f3444e57898d7352b
tonyhayes <tony.magellan@gmail.com>
2014-04-02 16:04:24 -0500

    added CRM repository interfaces

---
+++ src/main/java/com/drillmap/crm/repository/Opportunity.java
@@ -0,0 +1,10 @@
+package com.drillmap.crm.repository;
+
+import com.drillmap.crm.domain.entities.OpportunityForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by anthonyhayes on 4/2/14.
+ */
+public interface Opportunity extends JpaRepository<OpportunityForm, Long> {
+}
---
+++ src/main/java/com/drillmap/crm/repository/OpportunityFormComponent.java
@@ -0,0 +1,9 @@
+package com.drillmap.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by anthonyhayes on 4/2/14.
+ */
+public interface OpportunityFormComponent extends JpaRepository<OpportunityFormComponent, Long> {
+}
