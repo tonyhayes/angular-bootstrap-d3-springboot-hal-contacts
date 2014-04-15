16770c75c776a17b5993b29fd0fa1b6d23c5cc79
frensley <shane@drillmap.com>
2014-04-15 15:49:47 -0500

    [CRM-52] Pageable support for search methods

--- src/main/java/com/drillmap/crm/repository/CompanyRepository.java
+++ src/main/java/com/drillmap/crm/repository/CompanyRepository.java
@@ -17,11 +17,10 @@ public interface CompanyRepository extends JpaRepository<Company,Long>{
             @Param(value = "companyName") String companyName,
             @Param(value = "city") String city,
             @Param(value = "state") String state,
-            @Param(value = "contactName") String contactName,
-            Pageable page);
+            @Param(value = "contactName") String contactName, Pageable page);
 
-    public List<Company> findByCompanyNameLike(
-            @Param(value = "companyName") String companyName);
+    public Page<Company> findByCompanyNameLike(
+            @Param(value = "companyName") String companyName, Pageable page);
 
 
 }
--- src/main/java/com/drillmap/crm/repository/ContactRepository.java
+++ src/main/java/com/drillmap/crm/repository/ContactRepository.java
@@ -1,9 +1,13 @@
 package com.drillmap.crm.repository;
 
+import com.drillmap.crm.domain.entities.Company;
 import com.drillmap.crm.domain.entities.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -11,12 +15,15 @@ import java.util.List;
  */
 public interface ContactRepository extends JpaRepository<Contact,Long> {
 
-    public List<Contact> findByFirstName(@Param(value = "firstName") String firstName);
-    public List<Contact> findByLastName(@Param(value = "lastName") String lastName);
-
-    public List<Contact> findByFirstNameOrLastNameOrCityOrState(@Param(value = "firstName") String firstName,
+    public Page<Contact> findByFirstName(@Param(value = "firstName") String firstName, Pageable page);
+    public Page<Contact> findByLastName(@Param(value = "lastName") String lastName, Pageable page);
+    public Page<Contact> findByCompany(@Param(value = "company") Company company, Pageable page);
+    public Page<Contact> findByFirstNameOrLastNameOrCityOrState(@Param(value = "firstName") String firstName,
                                                                      @Param(value = "lastName") String lastName,
                                                                      @Param(value = "city") String city,
-                                                                     @Param(value = "state") String state);
+                                                                     @Param(value = "state") String state,
+                                                                     Pageable page
+
+    );
 
 }
