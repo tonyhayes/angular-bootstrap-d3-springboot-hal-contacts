242f3afb021605525e2eeacbaec827fff6733bb7
tonyhayes <tony.magellan@gmail.com>
2014-05-20 16:27:10 -0500

    [crm-62]initial charting solution -- very WIP

--- src/main/resources/static/app/js/controllers/chartsController.js
+++ src/main/resources/static/app/js/controllers/chartsController.js
@@ -10,24 +10,61 @@ angular.module('customersApp.chartsController', [])
  //           $scope.options = ChartService.chart.discreteBarChart;
             $scope.options = ChartService.chart.lineChart;
 
+            $scope.chartType = function (type) {
 
+                $scope.options = ChartService.chart[type];
+                $scope.data = data[type];
 
-//            $scope.data = [
-//                {
-//                    key: "Cumulative Return",
-//                    values: [
-//                        { "label": "A", "value": -29.765957771107 },
-//                        { "label": "B", "value": 0 },
-//                        { "label": "C", "value": 32.807804682612 },
-//                        { "label": "D", "value": 196.45946739256 },
-//                        { "label": "E", "value": 0.19434030906893 },
-//                        { "label": "F", "value": -98.079782601442 },
-//                        { "label": "G", "value": -13.925743130903 },
-//                        { "label": "H", "value": -5.1387322875705 }
-//                    ]
-//                }
-//            ]
-//
+            };
+
+            var data = {};
+            data.discreteBarChart =  [
+                {
+                    key: "Cumulative Return",
+                    values: [
+                        { "label": "A", "value": -29.765957771107 },
+                        { "label": "B", "value": 0 },
+                        { "label": "C", "value": 32.807804682612 },
+                        { "label": "D", "value": 196.45946739256 },
+                        { "label": "E", "value": 0.19434030906893 },
+                        { "label": "F", "value": -98.079782601442 },
+                        { "label": "G", "value": -13.925743130903 },
+                        { "label": "H", "value": -5.1387322875705 }
+                    ]
+                }
+            ];
+            data.lineChart = sinAndCos();
+            data.pieChart = [
+                {
+                    key: "One",
+                    y: 5
+                },
+                {
+                    key: "Two",
+                    y: 2
+                },
+                {
+                    key: "Three",
+                    y: 9
+                },
+                {
+                    key: "Four",
+                    y: 7
+                },
+                {
+                    key: "Five",
+                    y: 4
+                },
+                {
+                    key: "Six",
+                    y: 3
+                },
+                {
+                    key: "Seven",
+                    y: .5
+                }
+            ];
+            data.donutChart = data.pieChart;
 
 
             $scope.data = sinAndCos();
