61838e19a41a20510e711b0230a39cd458826548
frensley <shane@drillmap.com>
2014-10-24 13:19:38 -0500

    [DM-1683] Add pre-filter to check the existence of the sso cookie and invalidate session if not cookie present.

---
+++ src/main/java/com/drillmap/crm/security/legacy/filter/RememberMeSessionInvalidatorFilter.java
@@ -0,0 +1,56 @@
+package com.drillmap.crm.security.legacy.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sfrensley on 10/24/14.
+ */
+
+@Slf4j
+@Component
+public class RememberMeSessionInvalidatorFilter  implements Filter {
+
+
+    @Value("${sso.cookie.name}")
+    String cookieName;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            boolean found = false;
+            for (Cookie ck : cookies) {
+                if (cookieName.toLowerCase().equals(ck.getName().toLowerCase())) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                log.info("No SSO cookie found.");
+                req.getSession().invalidate();
+            }
+        }
+
+        req.getSession().invalidate();
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
